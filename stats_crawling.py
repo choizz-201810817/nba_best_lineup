d14f24070c91293b4616770489348216e01a3d16
choizz_201810817 <62290646+choizz-201810817@users.noreply.github.com>
2023-01-05 19:49:44 +0900

    01051949

---
+++ stats_crawling.py
@@ -0,0 +1,77 @@
+#%%
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+import pandas as pd
+import numpy as np
+
+#%%
+# 테이블의 body부분 크롤링
+def body(driver):
+    stats = []
+
+    while (True):
+        driver.execute_script("window.scrollTo(0, 400)") # 버튼이 보이는 곳까지 스크롤 내리기
+        driver.implicitly_wait(5)
+
+        nextButton = driver.find_element(By.XPATH, '//*[@id="__next"]/div[2]/div[2]/div[3]/section[2]/div/div[2]/div[2]/div[1]/div[5]/button[2]')
+        table = driver.find_element(By.XPATH, f'//*[@id="__next"]/div[2]/div[2]/div[3]/section[2]/div/div[2]/div[3]/table')
+        
+        # 테이블 body 가져오기
+        tbody = table.find_element(By.TAG_NAME, "tbody")
+
+        # 테이블 행 모두 가져오기
+        rows = tbody.find_elements(By.TAG_NAME, "tr")
+
+        # 각 행에서 값 모두 뽑아서 저장
+        for row in rows:
+            values = row.find_elements(By.TAG_NAME, "td")
+            stat = [i.text for i in values]
+
+            stats.append(stat)
+
+        if nextButton.is_enabled():
+            nextButton.click()
+        else:
+            break
+
+    return stats
+
+# %%
+seasons = ["1999-00", "2000-01", "2001-02", "2002-03", "2003-04", "2004-05", "2005-06"
+, "2006-07", "2007-08", "2008-09", "2009-10", "2010-11", "2011-12", "2012-13", "2013-14"
+, "2014-15", "2015-16", "2016-17", "2017-18", "2018-19", "2019-20", "2020-21", "2021-22"
+, "2022-23"]
+
+url = f"https://www.nba.com/stats/leaders?Season="
+
+driver = webdriver.Chrome()
+playerDfs = []
+
+for season in seasons:
+    driver.get(url+season)
+
+    table = driver.find_element(By.XPATH, f'//*[@id="__next"]/div[2]/div[2]/div[3]/section[2]/div/div[2]/div[3]/table')
+
+    # 테이블 헤드 가져오기
+    thead = table.find_element(By.TAG_NAME, "thead")
+    head_row = thead.find_element(By.TAG_NAME, "tr")
+    cols = head_row.find_elements(By.TAG_NAME, "th")
+
+    columns = []
+    for col in cols:
+        columns.append(col.text)
+
+    # 테이블 바디 가져오기
+    stats = body(driver)
+
+    df = pd.DataFrame(stats, columns=columns)
+    playerDfs.append(df)
+
+# %%
+for df,season in zip(playerDfs, seasons):
+    df.to_csv(f"./player_stats/{season}.csv")
+# %%
