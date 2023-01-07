d9b9aa8b2f511bac3fcb3b7b777e184e5785d4fd
choizz_201810817 <62290646+choizz-201810817@users.noreply.github.com>
2023-01-08 03:56:03 +0900

    0108_0355

--- stats_crawling.py
+++ stats_crawling.py
@@ -14,8 +14,8 @@ def body(driver):
     stats = []
 
     while (True):
-        driver.execute_script("window.scrollTo(0, 400)") # 버튼이 보이는 곳까지 스크롤 내리기
-        driver.implicitly_wait(5)
+        driver.execute_script("window.scrollTo(0, 350)") # 버튼이 보이는 곳까지 스크롤 내리기
+        driver.implicitly_wait(7) # 테이블 생성 기다리기
 
         nextButton = driver.find_element(By.XPATH, '//*[@id="__next"]/div[2]/div[2]/div[3]/section[2]/div/div[2]/div[2]/div[1]/div[5]/button[2]')
         table = driver.find_element(By.XPATH, f'//*[@id="__next"]/div[2]/div[2]/div[3]/section[2]/div/div[2]/div[3]/table')
@@ -31,7 +31,7 @@ def body(driver):
             values = row.find_elements(By.TAG_NAME, "td")
             stat = [i.text for i in values]
 
-            stats.append(stat)
+            stats.append(stat) # 각 행이 리스트로 묶여 stats 리스트에 append됨(2차원 list 생성)
 
         if nextButton.is_enabled():
             nextButton.click()
@@ -41,37 +41,57 @@ def body(driver):
     return stats
 
 # %%
+# generals = ["scoring", "usage", "defense", "estimated-advanced"]
+generals = ["traditional", "advanced", "scoring"]
+
 seasons = ["1999-00", "2000-01", "2001-02", "2002-03", "2003-04", "2004-05", "2005-06"
 , "2006-07", "2007-08", "2008-09", "2009-10", "2010-11", "2011-12", "2012-13", "2013-14"
 , "2014-15", "2015-16", "2016-17", "2017-18", "2018-19", "2019-20", "2020-21", "2021-22"
 , "2022-23"]
 
-url = f"https://www.nba.com/stats/leaders?Season="
-
-driver = webdriver.Chrome()
 playerDfs = []
 
-for season in seasons:
-    driver.get(url+season)
+driver = webdriver.Chrome()
+for general in generals:
+    url = f"https://www.nba.com/stats/players/{general}?Season="
 
-    table = driver.find_element(By.XPATH, f'//*[@id="__next"]/div[2]/div[2]/div[3]/section[2]/div/div[2]/div[3]/table')
+    for season in seasons:
+        driver.get(url+season)
+        driver.refresh()
+        driver.implicitly_wait(15)
+        table = driver.find_element(By.XPATH, f'//*[@id="__next"]/div[2]/div[2]/div[3]/section[2]/div/div[2]/div[3]/table')
 
-    # 테이블 헤드 가져오기
-    thead = table.find_element(By.TAG_NAME, "thead")
-    head_row = thead.find_element(By.TAG_NAME, "tr")
-    cols = head_row.find_elements(By.TAG_NAME, "th")
+        # 테이블 헤드 가져오기
+        thead = table.find_element(By.TAG_NAME, "thead")
+        head_row = thead.find_element(By.TAG_NAME, "tr")
+        cols = head_row.find_elements(By.TAG_NAME, "th")
 
-    columns = []
-    for col in cols:
-        columns.append(col.text)
+        columns = []
+        for col in cols:
+            columns.append(col.text)
+        while ('' in columns):
+            columns.remove('') # ''로 나오는 컬럼명 없애기
+        columns = columns[1:]
+        columns.insert(0,'#')
 
-    # 테이블 바디 가져오기
-    stats = body(driver)
+        # 테이블 바디 가져오기
+        stats = body(driver)
 
-    df = pd.DataFrame(stats, columns=columns)
-    playerDfs.append(df)
+        df = pd.DataFrame(stats, columns=columns)
+        df["season"] = "'"+season[2:]
+        df.columns = df.columns.str.lower()
+        playerDfs.append(df)
 
 # %%
-for df,season in zip(playerDfs, seasons):
-    df.to_csv(f"./player_stats/{season}.csv")
-# %%
+for i, general in enumerate(generals):
+    df = pd.concat(playerDfs[i*24:(i+1)*24])
+    df.to_csv(f'./data/{general}/player_{general}.csv')
+
+#%%
+# playerDfs1 = playerDfs[:24]
+# # %%
+# for df,season in zip(playerDfs1, seasons):
+#     df.to_csv(f"./data/misc/player_stats/player_{season}.csv")
+# # %%
+# df = pd.concat(playerDfs1).reset_index(drop=True)
+# df.to_csv('./data/player_misc.csv')
--- team_crawling.py
+++ team_crawling.py
@@ -12,6 +12,10 @@ import numpy as np
 # 테이블의 body부분 크롤링
 def body(driver):
     stats = []
+
+    driver.execute_script("window.scrollTo(0, 200)")
+    driver.implicitly_wait(7) # 테이블 생성 기다리기
+
     table = driver.find_element(By.XPATH, f'//*[@id="__next"]/div[2]/div[2]/div[3]/section[2]/div/div[2]/div[3]/table')
     
     # 테이블 body 가져오기
@@ -26,45 +30,62 @@ def body(driver):
         stat = [i.text for i in values]
 
         stats.append(stat)
-    
+
     return stats
 
 # %%
+generals = ["four-factors", "misc", "defense", "estimated-advanced"]
+# generals = ["traditional", "advanced", "scoring"]
+
 seasons = ["1999-00", "2000-01", "2001-02", "2002-03", "2003-04", "2004-05", "2005-06"
 , "2006-07", "2007-08", "2008-09", "2009-10", "2010-11", "2011-12", "2012-13", "2013-14"
 , "2014-15", "2015-16", "2016-17", "2017-18", "2018-19", "2019-20", "2020-21", "2021-22"
 , "2022-23"]
 
-url = f"https://www.nba.com/stats/teams/traditional?Season="
-
-driver = webdriver.Chrome()
 teamDfs = []
 
-for season in seasons:
-    driver.get(url+season)
-    driver.implicitly_wait(2)
-    driver.execute_script("window.scrollTo(0, 400)")
+driver = webdriver.Chrome()
+for general in generals:
+    url = f"https://www.nba.com/stats/teams/{general}?Season="
 
-    table = driver.find_element(By.XPATH, f'//*[@id="__next"]/div[2]/div[2]/div[3]/section[2]/div/div[2]/div[3]/table')
+    for season in seasons:
+        driver.get(url+season)
+        driver.refresh() # 표 안뜨는 경우를 대비하여 새로고침
+        driver.implicitly_wait(15)
+        table = driver.find_element(By.XPATH, f'//*[@id="__next"]/div[2]/div[2]/div[3]/section[2]/div/div[2]/div[3]/table')
 
-    # 테이블 헤드 가져오기
-    thead = table.find_element(By.TAG_NAME, "thead")
-    head_row = thead.find_element(By.TAG_NAME, "tr")
-    cols = head_row.find_elements(By.TAG_NAME, "th")
+        # 테이블 헤드 가져오기
+        thead = table.find_element(By.TAG_NAME, "thead")
+        head_row = thead.find_element(By.TAG_NAME, "tr")
+        cols = head_row.find_elements(By.TAG_NAME, "th")
 
-    columns = []
-    for col in cols:
-        columns.append(col.text)
-    columns = columns[1:28]
-    columns.insert(0,'#')
+        columns = []
+        for col in cols:
+            columns.append(col.text)
+        while ('' in columns):
+            columns.remove('') # ''로 나오는 컬럼명 없애기
+        # columns = columns[1:]
+        columns.insert(0,'#')
 
-    driver.implicitly_wait(2)
-    # 테이블 바디 가져오기
-    stats = body(driver)
+        # 테이블 바디 가져오기
+        stats = body(driver)
 
-    df = pd.DataFrame(stats, columns=columns)
-    teamDfs.append(df)
+        df = pd.DataFrame(stats, columns=columns)
+        df["season"] = "'"+season[2:]
+        df.columns = df.columns.str.lower()
+        teamDfs.append(df)
 
 # %%
-for df,season in zip(teamDfs, seasons):
-    df.to_csv(f"./team_stats/{season}.csv")
+for i, general in enumerate(generals):
+    df = pd.concat(teamDfs[i*24:(i+1)*24])
+    df.to_csv(f'./data/{general}/team_{general}.csv')
+
+# teamDfs1 = teamDfs[:24]
+# # %%
+# for df,season in zip(teamDfs1, seasons):
+#     df.to_csv(f"./data/misc/team_stats/team_{season}.csv")
+# # %%
+# df = pd.concat(teamDfs1).reset_index(drop=True)
+# df.to_csv('./data/team_misc.csv')
+
+# %%
